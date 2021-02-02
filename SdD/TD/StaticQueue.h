b308f6482f59aab66e81ad04978a004e93882133
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2021-02-02 20:34:27 +0100

    Create StaticQueue.h

---
+++ SdD/TD/StaticQueue.h
@@ -0,0 +1,24 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__ 
+
+//Définiton implicite de la file
+typedef struct s_queue* Queue;
+
+//Opérateurs constructeurs
+Queue queue();
+Queue push(Queue, int);
+//Opérateurs
+Queue pop(Queue);
+int top(Queue);
+bool isEmpty(Queue);
+
+/**
+ * Propriétés de la file :
+ * - isEmpty(queue) = true
+ * - isEmpty(push(queue, e)) = false
+ * - isEmpty(pop(push(queue, e))) = true
+ */
+
+typedef enum { false = 0, true = 1}bool;
+
+#endif
