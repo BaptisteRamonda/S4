ca25df02eac7086aeca27534c058511f8e024387
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2021-02-03 15:42:59 +0100

    Delete IFileNonBornee.java

--- POO/TP/TP2/IFileNonBornee.java
+++
@@ -1,9 +0,0 @@
-package tp2;
-
-public interface IFileNonBornee extends IFile {
-    /**
-     * ajoute un élément en queue de file
-     * param e: référence de l'élément à ajouter
-     */
-    public boolean add(double e);
-}
