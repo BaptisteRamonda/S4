02547dcb128f5742d4d86ff23f5977b4d135533c
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2021-02-03 15:09:05 +0100

    Update IFileNonBornee.java

--- POO/TP/TP2/IFileNonBornee.java
+++ POO/TP/TP2/IFileNonBornee.java
@@ -5,5 +5,5 @@ public interface IFileNonBornee extends IFile {
      * ajoute un élément en queue de file
      * param e: référence de l'élément à ajouter
      */
-    public void add(double e);
+    public boolean add(double e);
 }
