dbc2cfe9dac572ecdf15a8284dbadd39820c94f0
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2021-02-16 16:08:04 +0100

    Create stack.h

---
+++ SdD/TD/Stack/stack.h
@@ -0,0 +1,16 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+//Constantes
+#define STACK_SIZE 16
+
+//Définiton opaque de la structure de la pile
+typedef struct s_stack* Stack;
+
+//Prototypes
+/**bool isEmpty(Stack);
+void pop(Stack);
+void push(Stack, int)
+int top(Stack); */
+
+#endif
